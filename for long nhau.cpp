0d667d8a275527cf70447630bcc624ac341e6757
aboloco <thanhson16102005@gmail.com>
2024-09-19 23:25:38 +0700

    fuck manguyenvu

---
+++ for long nhau.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+#include<cmath>
+#include <cstring>
+int calculation(double a, double b, char operation);
+int main()
+{
+	double x, y;
+	char z;
+	std::cout << "Please enter your X : ";
+	std::cin >> x;
+	std::cout << "\nPlease enter your Y : ";
+	std::cin >> y;
+	std::cout << "\nPlease choose the operation you wanted to solve (+,-,*,/) : ";
+	std::cin >> z;
+	calculation(x, y, z);
+}
+int calculation(double a, double b, char operation)
+{
+	double solution;
+	switch (operation)
+	{
+	case '+':
+		solution = a + b;
+		std::cout << "Your answer is : " << solution;
+		break;
+	case'-':
+		solution = a - b;
+		std::cout << "Your answer is : " << solution;
+		break;
+	case'*':
+		solution = a * b;
+		std::cout << "Your answer is : " << solution;
+		break;
+	case'/':
+		solution = a / b;
+		std::cout << "Your answer is : " << solution;
+		break;
+	default:
+		std::cout << "Your operation doesn't exist!";
+		break;
+	}
+	return 0;
+}
